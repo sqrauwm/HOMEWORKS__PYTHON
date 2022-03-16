7c601d8deb1fb7b28a5ad14b8f166147459cd8bb
Michał <97949139+sqrauwm@users.noreply.github.com>
2022-03-16 10:28:42 +0100

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,12 @@
+zadanieA=[1-x for x in range(1,11)]
+print(zadanieA)
+
+################################################
+
+zadanieB=zadanieB=[4**x for x in range(0,8)]
+print(zadanieB)
+
+################################################
+
+zadanieC=[x for x in zadanieB if x%2==0]
+print(zadanieC)
