3909fde9dce7c8a67e6581a69b54c2d2494dd9fd
Michał <97949139+sqrauwm@users.noreply.github.com>
2022-04-27 10:29:00 +0200

    Add files via upload

---
+++ homework9/main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Zadanie 1
+
+# xlsx = pd.ExcelFile('imiona.xlsx')
+# df = pd.read_excel(xlsx, header=0)
+# grupa = df.groupby('Rok').agg({'Liczba':['sum']})
+# grupa.plot(kind='line', xlabel='Rok', ylabel='Ilosc imion',
+#             rot=0, title='Ilosc imion w danym roku')
+# plt.show()
+
+# Zadanie 2
+
+# xlsx = pd.ExcelFile('imiona.xlsx')
+# df = pd.read_excel(xlsx, header=0)
+# grupa = df.groupby('Plec').agg({'Liczba':['sum']})
+# grupa.plot(kind='bar', xlabel='Płeć', ylabel='Ilośc imion w mln',
+#             rot=0, title='Ilość imion w zależności od płci')
+# plt.show()
+
+# Zadanie 3 ????
